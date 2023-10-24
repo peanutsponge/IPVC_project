e326328c9c96c56e0b85d4b56b9902042c19f3e3
gijsdesmit <g.desmit@student.utwente.nl>
2023-10-24 13:14:10 +0200

    Added support in main for camera_calibration package
    Now the file 'calibration_data.pkl' is created and loaded.
    TODO: Need to discuss how to deal with stereo rectification in main

--- camera_calibration.py
+++ camera_calibration.py
@@ -2,7 +2,7 @@
 import cv2 as cv
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+import pickle
 
 # Global parameters
 ch_rows = 6  # rows of checkerboard
@@ -55,7 +55,7 @@ def calibrate_camera(images_, camera_name):
     #print('distortion coeffs:', dist)
     # print('Rs:\n', rvecs)
     # print('Ts:\n', tvecs)
-    return mtx, dist, corners_img, corners_3d
+    return ret, mtx, dist, rvecs, tvecs, corners_img, corners_3d
 
 def stereo_calibrate(mtx1, dist1, mtx2, dist2, images1_, images2_, camera_name1, camera_name2):
     images1 = deep_copy_images(images1_) #to prevent drawing on the original images
@@ -105,25 +105,17 @@ def stereo_calibrate(mtx1, dist1, mtx2, dist2, images1_, images2_, camera_name1,
                                                                  mtx2, dist2, (width, height), criteria=criteria, flags=stereocalibration_flags)
     print('\tStereo calibration metrics:')
     print('\trmse:', ret)
-    return R, T
+    return ret, CM1, dist1, CM2, dist2, R, T, E, F
 
+def apply_lens_correction(image, camera_name, calibration_data):
+    #probably not necessary to call this because stereo_rectify already calls initUndistortRectifyMap
+    if camera_name not in ['left', 'middle', 'right']:
+        print('Error in function call apply_lens_correction: camera_name should be one of: left, middle, right')
+        return None
 
-# Load images from the "Calibration 1/calibration.." folder
-left_images, middle_images, right_images = [], [], []
-readImages(left_images,'Calibratie 1'+os.path.sep+'calibrationLeft')
-readImages(middle_images,'Calibratie 1'+os.path.sep+'calibrationMiddle')
-readImages(right_images,'Calibratie 1'+os.path.sep+'calibrationRight')
-
-# Calibrate each camera individually
-mtx_left, dist_left, corners_img_left, corners_3d_left = calibrate_camera(left_images, 'left')
-mtx_middle, dist_middle, corners_img_middle, corners_3d_middle = calibrate_camera(middle_images, 'middle')
-mtx_right, dist_right, corners_img_right, corners_3d_right = calibrate_camera(right_images, 'right')
-
-# Stereo calibrate the 2 pairs of cameras
-R_lm, T_lm = stereo_calibrate(mtx_left, dist_left, mtx_middle, dist_middle, left_images, middle_images, 'left', 'middle')
-R_mr, T_mr = stereo_calibrate(mtx_middle, dist_middle, mtx_right, dist_right, middle_images, right_images, 'middle', 'right')
-
-#TODO: Next we need perform stereo rectification on the images, i think we could use the cv.stereoRectify function for this.
+    # Undistort the image
+    corrected_image = cv.undistort(image, calibration_data['mtx_'+camera_name], calibration_data['dist_'+camera_name], None, calibration_data['mtx_'+camera_name])
+    return corrected_image
 
 def stereo_rectify(mtx1, dist1, mtx2, dist2, R, T, width, height):
     # Compute the rectification transformations
@@ -135,13 +127,6 @@ def stereo_rectify(mtx1, dist1, mtx2, dist2, R, T, width, height):
 
     return R1, R2, P1, P2, Q, map1x, map1y, map2x, map2y
 
-width = left_images[0].shape[1]
-height = left_images[0].shape[0]
-
-# stereo_rectify function for left and middle cameras
-R1_lm, R2_lm, P1_lm, P2_lm, Q_lm, map1x_lm, map1y_lm, map2x_lm, map2y_lm = stereo_rectify(mtx_left, dist_left, mtx_middle, dist_middle, R_lm, T_lm, width, height)
-# stereo_rectify function for middle and right cameras
-R1_mr, R2_mr, P1_mr, P2_mr, Q_mr, map1x_mr, map1y_mr, map2x_mr, map2y_mr = stereo_rectify(mtx_middle, dist_middle, mtx_right, dist_right, R_mr, T_mr, width, height)
 
 # Rectify a pair of images, see cc_testing.ipynb for an example
 def rectify_images(image1, image2, map1x, map1y, map2x, map2y):
@@ -150,3 +135,112 @@ def rectify_images(image1, image2, map1x, map1y, map2x, map2y):
     rectified_image2 = cv.remap(image2, map2x, map2y, interpolation=cv.INTER_LINEAR)
     return rectified_image1, rectified_image2
 
+#This function doesn't have to be used
+def rectify_image(image, map1x, map1y):
+    # Apply rectification maps to the images
+    rectified_image = cv.remap(image, map1x, map1y, interpolation=cv.INTER_LINEAR)
+    return rectified_image
+
+def calibrate_3_cameras_to_file(filename='calibration_data.pkl'):
+    print('--> Obtaining calibration data...')
+    # Load images from the "Calibration 1/calibration.." folder
+    left_images, middle_images, right_images = [], [], []
+    readImages(left_images,'Calibratie 1'+os.path.sep+'calibrationLeft')
+    readImages(middle_images,'Calibratie 1'+os.path.sep+'calibrationMiddle')
+    readImages(right_images,'Calibratie 1'+os.path.sep+'calibrationRight')
+
+    # Calibrate each camera individually
+    ret_left, mtx_left, dist_left, rvecs_left, tvecs_left, corners_img_left, corners_3d_left = calibrate_camera(left_images, 'left')
+    ret_middle, mtx_middle, dist_middle, rvecs_middle, tvecs_middle, corners_img_middle, corners_3d_middle = calibrate_camera(middle_images, 'middle')
+    ret_right, mtx_right, dist_right, rvecs_right, tvecs_right, corners_img_right, corners_3d_right = calibrate_camera(right_images, 'right')
+
+    # Stereo calibrate the 2 pairs of cameras
+    ret_lm, CM1_lm, dist1_lm, CM2_lm, dist2_lm, R_lm, T_lm, E_lm, F_lm = stereo_calibrate(mtx_left, dist_left, mtx_middle, dist_middle, left_images, middle_images, 'left', 'middle')
+    ret_mr, CM1_mr, dist1_mr, CM2_mr, dist2_mr, R_mr, T_mr, E_mr, F_mr = stereo_calibrate(mtx_middle, dist_middle, mtx_right, dist_right, middle_images, right_images, 'middle', 'right')
+
+    width = left_images[0].shape[1]
+    height = left_images[0].shape[0]
+
+    # stereo_rectify function for left and middle cameras
+    R1_lm, R2_lm, P1_lm, P2_lm, Q_lm, map1x_lm, map1y_lm, map2x_lm, map2y_lm = stereo_rectify(mtx_left, dist_left, mtx_middle, dist_middle, R_lm, T_lm, width, height)
+    # stereo_rectify function for middle and right cameras
+    R1_mr, R2_mr, P1_mr, P2_mr, Q_mr, map1x_mr, map1y_mr, map2x_mr, map2y_mr = stereo_rectify(mtx_middle, dist_middle, mtx_right, dist_right, R_mr, T_mr, width, height)
+
+    data = {
+        'width': width, #widht of frame
+        'height': height, #height of frame
+        'ret_left': ret_left, #ret is the root-mean-square error
+        'mtx_left': mtx_left, #camera matrix
+        'dist_left': dist_left, #distortion coefficients
+        'rvecs_left': rvecs_left, #rotation vectors
+        'tvecs_left': tvecs_left, #translation vectors
+        'corners_img_left': corners_img_left, #pixel coordinates of checkerboard corners
+        'corners_3d_left': corners_3d_left, #coordinates of the checkerboard in checkerboard world space
+        'ret_middle': ret_middle,
+        'mtx_middle': mtx_middle,
+        'dist_middle': dist_middle,
+        'rvecs_middle': rvecs_middle,
+        'tvecs_middle': tvecs_middle,
+        'corners_img_middle': corners_img_middle,
+        'corners_3d_middle': corners_3d_middle,
+        'ret_right': ret_right,
+        'mtx_right': mtx_right,
+        'dist_right': dist_right,
+        'rvecs_right': rvecs_right,
+        'tvecs_right': tvecs_right,
+        'corners_img_right': corners_img_right,
+        'corners_3d_right': corners_3d_right,
+        'ret_lm': ret_lm,
+        'CM1_lm': CM1_lm, #camera matrix l for stereo calibration
+        'dist1_lm': dist1_lm,
+        'CM2_lm': CM2_lm, #camera matrix m for stereo calibration
+        'dist2_lm': dist2_lm,
+        'R_lm': R_lm, #rotation matrix for stereo calibration
+        'T_lm': T_lm, #translation vector for stereo calibration
+        'E_lm': E_lm, #essential matrix
+        'F_lm': F_lm, #fundamental matrix
+        'ret_mr': ret_mr,
+        'CM1_mr': CM1_mr,
+        'dist1_mr': dist1_mr,
+        'CM2_mr': CM2_mr,
+        'dist2_mr': dist2_mr,
+        'R_mr': R_mr,
+        'T_mr': T_mr,
+        'E_mr': E_mr,
+        'F_mr': F_mr,
+        #for stereo rectification:
+        'R1_lm': R1_lm, #rectification transformation for left camera
+        'R2_lm': R2_lm, #rectification transformation for middle camera
+        'P1_lm': P1_lm, #projection matrix in the new (rectified) coordinate systems for left camera
+        'P2_lm': P2_lm, #projection matrix in the new (rectified) coordinate systems for middle camera
+        'Q_lm': Q_lm, #disparity-to-depth mapping matrix
+        'map1x_lm': map1x_lm, #rectification map x for left camera
+        'map1y_lm': map1y_lm, #rectification map y for left camera
+        'map2x_lm': map2x_lm, #rectification map x for middle camera
+        'map2y_lm': map2y_lm, #rectification map x for middle camera
+        'R1_mr': R1_mr,
+        'R2_mr': R2_mr,
+        'P1_mr': P1_mr,
+        'P2_mr': P2_mr,
+        'Q_mr': Q_mr,
+        'map1x_mr': map1x_mr,
+        'map1y_mr': map1y_mr,
+        'map2x_mr': map2x_mr,
+        'map2y_mr': map2y_mr
+    }
+    #Save all variables in a file
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
+    print('--> Calibration data saved to file calibration_data.pkl')
+
+def get_calibration_data_from_file(filename='calibration_data.pkl'):
+    #Load all variables from the file
+    with open(filename, 'rb') as file:
+        data = pickle.load(file) #data is a dictionary
+    return data
+
+if __name__ == '__main__':
+    calibrate_3_cameras_to_file()
+    data = get_calibration_data_from_file()
+    #print(data)
+    print('main of camera_calibration.py executed')
--- main.py
+++ main.py
@@ -9,9 +9,16 @@ from remove_background import get_foreground_mask
 from get_images import getTriplet
 from mesh import generate_mesh
 from normalise_color import normalize_global_color
+from camera_calibration import get_calibration_data_from_file, calibrate_3_cameras_to_file, apply_lens_correction, rectify_images
 
+# Only execute to generate the calibration data file
+#calibrate_3_cameras_to_file('calibration_data.pkl')
 
-# camera_parameters = get_camera_parameters()
+# Load the calibration data, see camera_calibration.py for more info on the specific saved dictionary entries
+calibration_data = get_calibration_data_from_file('calibration_data.pkl')
+
+# Camera id mappings
+camera_id = {0: "left", 1: "middle", 2: "right"}
 
 mask = [None, None, None]
 triplet = getTriplet(1, 0)  # In final version we'll loop over these
@@ -19,14 +26,19 @@ for i, im in enumerate(triplet):
     # Make Foreground mask
     mask[i] = get_foreground_mask(im)
     # Compensation for Non-linear lens deformation
-    im = im
-    # Stereo rectification to facilitate the dense stereo matching
-    im = im
+    im = apply_lens_correction(im, camera_id[i], calibration_data)
+    #show the image
+
     # global colour normalization to make sure that the so-called ‘Constant Brightness Assumption’ holds true.
     im = normalize_global_color(im)
 
     # Save the image to the triplet
     triplet[i] = im
+
+#TODO: Where to put this? In the loop or after the loop? Doesn't really work with the loop
+# Stereo rectification to facilitate the dense stereo matching
+rect_images_LM = (triplet[0], triplet[1], calibration_data['map1x_lm'], calibration_data['map1y_lm'], calibration_data['map2x_lm'], calibration_data['map2y_lm'])
+rect_images_MR = (triplet[1], triplet[2], calibration_data['map1x_mr'], calibration_data['map1y_mr'], calibration_data['map2x_mr'], calibration_data['map2y_mr'])
 
 # Display the images
 cv.imshow('Left', triplet[0])
@@ -39,6 +51,7 @@ mesh_LM = generate_mesh(im[0], im[1], mask[0], mask[1])
 mesh_MR = generate_mesh(im[1], im[2], mask[1], mask[2])
 
 # Merge the meshes using the ICP algorithm (iterated closest points)
+
 
 # Save the mesh to a file
 
