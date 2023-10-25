5de9e01311131e116f3ea0df8e5f2df2eb460166
Damian <damiankagenaar@gmail.com>
2023-10-25 11:42:25 +0200

    first commit

--- main.py
+++ main.py
@@ -9,10 +9,24 @@ from remove_background import get_foreground_mask
 from get_images import getTriplet
 from mesh import generate_mesh
 from normalise_color import normalize_global_color
-from camera_calibration import get_calibration_data_from_file, calibrate_3_cameras_to_file, apply_lens_correction, rectify_images
+from camera_calibration import get_calibration_data_from_file, calibrate_3_cameras_to_file, apply_lens_correction, \
+    rectify_images
+
+
+def preprocess(images):
+    mask = [None, None]
+    # Stereo rectification to facilitate the dense stereo matching, also performs non-linear distortion correction!
+    for i in [0, 1]:
+        # Make Foreground mask
+        mask[i] = get_foreground_mask(images[i])
+
+        # global colour normalization to make sure that the so-called ‘Constant Brightness Assumption’ holds true.
+        images[i] = normalize_global_color(images[i])
+    return images, mask
+
 
 # Only execute to generate the calibration data file
-#calibrate_3_cameras_to_file('calibration_data.pkl')
+# calibrate_3_cameras_to_file('calibration_data.pkl')
 
 # Load the calibration data, see camera_calibration.py for more info on the specific saved dictionary entries
 calibration_data = get_calibration_data_from_file('calibration_data.pkl')
@@ -20,36 +34,27 @@ calibration_data = get_calibration_data_from_file('calibration_data.pkl')
 # Camera id mappings
 camera_id = {0: "left", 1: "middle", 2: "right"}
 
-mask = [None, None, None]
 triplet = getTriplet(1, 0)  # In final version we'll loop over these
-for i, im in enumerate(triplet):
-    # Make Foreground mask
-    mask[i] = get_foreground_mask(im)
 
-    # Apply lens correction, not necessary here! is performed in rectify_images
-    # im = apply_lens_correction(im, camera_id[i], calibration_data)
-    #show the image
-
-    # global colour normalization to make sure that the so-called ‘Constant Brightness Assumption’ holds true.
-    im = normalize_global_color(im)
-
-    # Save the image to the triplet
-    triplet[i] = im
-
-#TODO: Where to put this? In the loop or after the loop? Doesn't really work with the loop
 # Stereo rectification to facilitate the dense stereo matching, also performs non-linear distortion correction!
-rect_images_LM = (triplet[0], triplet[1], calibration_data['map1x_lm'], calibration_data['map1y_lm'], calibration_data['map2x_lm'], calibration_data['map2y_lm'])
-rect_images_MR = (triplet[1], triplet[2], calibration_data['map1x_mr'], calibration_data['map1y_mr'], calibration_data['map2x_mr'], calibration_data['map2y_mr'])
+rect_images_LM = rectify_images(triplet[0], triplet[1], calibration_data['map1x_lm'], calibration_data['map1y_lm'],
+                                calibration_data['map2x_lm'], calibration_data['map2y_lm'])
+rect_images_MR = rectify_images(triplet[1], triplet[2], calibration_data['map1x_mr'], calibration_data['map1y_mr'],
+                                calibration_data['map2x_mr'], calibration_data['map2y_mr'])
+
+# Preprocess the images
+images_LM, mask_LM = preprocess(rect_images_LM)
+images_MR, mask_MR = preprocess(rect_images_MR)
 
 # Display the images
-cv.imshow('Left', triplet[0])
-cv.imshow('Middle', triplet[1])
-cv.imshow('Right', triplet[2])
-cv.waitKey(0)
+# cv.imshow('Left', triplet[0])
+# cv.imshow('Middle', triplet[1])
+# cv.imshow('Right', triplet[2])
+# cv.waitKey(0)
 
 # Generate two meshes
-mesh_LM = generate_mesh(im[0], im[1], mask[0], mask[1])
-mesh_MR = generate_mesh(im[1], im[2], mask[1], mask[2])
+mesh_LM = generate_mesh(images_LM, mask_LM)
+mesh_MR = generate_mesh(images_MR, mask_MR)
 
 # Merge the meshes using the ICP algorithm (iterated closest points)
 
--- mesh.py
+++ mesh.py
@@ -1,21 +1,24 @@
 """
 This file contains the functions to generate a mesh from two images.
 """
-def generate_mesh(im1, im2, mask1, mask2):
+import cv2 as cv
+def generate_mesh(images, mask):
     """
     Use the two images to generate a mesh.
     The images have been stereo rectified, so the epipolar lines are horizontal.
     The images have been compensated for Non-linear lens deformation.
     https://docs.opencv.org/3.4/da/de9/tutorial_py_epipolar_geometry.html
-    :param mask2:
-    :param mask1:
-    :param im1:
-    :param im2:
+    :param mask: The mask to use to only use the foreground
+    :param images: The two images to generate the mesh from
     :return: The mesh
     """
     # Use the two images to find the fundamental matrix
 
     # Use stereo matching to find the disparity map
+    stereo = cv.StereoBM.create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(images[0], images[1])
+    # Use the disparity map to find the depth map
+    Z = 1 / disparity
 
     # Use the disparity map to find the depth map
 
