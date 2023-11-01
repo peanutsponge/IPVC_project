ba105cf5f17c080dd1d5b8143957b215dbf8ec19
Chris <86408925+ChrisBosman@users.noreply.github.com>
2023-11-01 15:58:35 +0100

    added poisson mesh

--- mesh.py
+++ mesh.py
@@ -5,6 +5,7 @@ import cv2 as cv
 import numpy as np
 import open3d as o3d
 from remove_background import region_fill
+import matplotlib.pyplot as plt
 
 def visualize_point_cloud_file(filename):
     point_cloud = o3d.io.read_point_cloud(filename)
@@ -60,9 +61,48 @@ def create_mesh(point_cloud, name, alpha = 0.02):
     #o3d.io.write_triangle_mesh("output/mesh_"+name+".stl", mesh)
     return mesh
 
-def visualize_mesh(mesh):
+def create_mesh_poisson(point_cloud, name):
+    # Estimate normals
+    point_cloud.normals = o3d.utility.Vector3dVector(np.zeros((1, 3)))  # Assign some temporary normals
+    point_cloud.estimate_normals()
+    # o3d.visualization.draw_geometries([point_cloud], point_show_normal=True)
+    # Create a mesh from the point cloud
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud, depth=8)
+    mesh.compute_vertex_normals()
+    mesh.compute_triangle_normals()
+    print('Remove mesh outliers (low density)')
+    vertices_to_remove = densities < np.quantile(densities, 0.01)
+    mesh.remove_vertices_by_mask(vertices_to_remove)
+
+
+    # print('Visualize densities')
+    # densities = np.asarray(densities)
+    # density_colors = plt.get_cmap('plasma')(
+    #     (densities - densities.min()) / (densities.max() - densities.min()))
+    # density_colors = density_colors[:, :3]
+    # density_mesh = o3d.geometry.TriangleMesh()
+    # density_mesh.vertices = mesh.vertices
+    # density_mesh.triangles = mesh.triangles
+    # density_mesh.triangle_normals = mesh.triangle_normals
+    # density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
+    # o3d.visualization.draw_geometries([density_mesh],
+    #                                 zoom=0.664,
+    #                                 front=[-0.4761, -0.4698, -0.7434],
+    #                                 lookat=[1.8900, 3.2596, 0.9284],
+    #                                 up=[0.2304, -0.8825, 0.4101])
+    # print('Done with mesh generation')
+    return mesh
+
+def visualize_mesh(mesh, pointcloud = None):
     # plot the mesh
-    o3d.visualization.draw_geometries([mesh])
+    if pointcloud == None:
+        o3d.visualization.draw_geometries([mesh])
+    else:
+        o3d.visualization.draw_geometries([pointcloud, mesh])
+
+def save_mesh(mesh, name):
+    # Save the mesh to a stl file
+    o3d.io.write_triangle_mesh("output/mesh_"+name+".stl", mesh)
 
 def compute_disparity_map_old(images, mask=None):
     """
@@ -262,7 +302,7 @@ def generate_point_cloud(rectified_images, foreground_masks, calibration_data, s
 
     Q = np.float32([[1, 0, 0, 0],
                    [0, -1, 0, 0],
-                   [0, 0, focal_length * 0.07, 0],
+                   [0, 0, focal_length * 0.1, 0],
                    [0, 0, 0, 1]])
     #Q = calibration_data[f'Q{suffix}'] # alternative, but sucks
 
