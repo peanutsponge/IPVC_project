d488342e872c71f12da8d5cf532b1429f33396e1
gijsdesmit <g.desmit@student.utwente.nl>
2023-11-01 18:48:03 +0100

    Support for all subjects

--- main.py
+++ main.py
@@ -9,33 +9,38 @@ from point_cloud_alignment import combine_point_clouds
 from mesh import create_mesh_poisson, visualize_mesh
 
 
-# Only execute to generate the calibration data file
-#calibrate_3_cameras_to_file('calibration_data.pkl')
+subjects = [1,2,4]
+numbers = [0,1,2,3]
+for s in subjects:
+    for n in numbers:
+        # Only execute to generate the calibration data file
+        #calibrate_3_cameras_to_file('calibration_data.pkl')
 
-# Load the calibration data, see camera_calibration.py for more info on the specific saved dictionary entries
-calibration_data = get_calibration_data_from_file('calibration_data.pkl')
+        # Load the calibration data, see camera_calibration.py for more info on the specific saved dictionary entries
+        calibration_data = get_calibration_data_from_file('calibration_data.pkl')
 
-# Load the images
-triplet = getTriplet(1, 0)  # In final version we'll loop over these
+        # Load the images
+        triplet = getTriplet(s, n)  # In final version we'll loop over these
 
-# Preprocess the images
-images_LM, mask_LM = preprocess([triplet[0], triplet[1]], calibration_data, "_lm", display=False)
-images_MR, mask_MR = preprocess([triplet[1], triplet[2]], calibration_data, "_mr", display=False)
-print('Finished preprocessing images')
+        # Preprocess the images
+        images_LM, mask_LM = preprocess([triplet[0], triplet[1]], calibration_data, "_lm", display=False)
+        images_MR, mask_MR = preprocess([triplet[1], triplet[2]], calibration_data, "_mr", display=False)
+        print('Finished preprocessing images')
 
-# Generate two point clouds
-pc_LM = generate_point_cloud(images_LM, mask_LM, calibration_data, "_lm", display_disparity=False, display_point_cloud=False)
-pc_RM = generate_point_cloud(images_MR, mask_MR, calibration_data, "_mr", display_disparity=False, display_point_cloud=False)
-print('Finished generating point clouds')
+        # Generate two point clouds
+        pc_LM = generate_point_cloud(images_LM, mask_LM, calibration_data, "_lm", display_disparity=False, display_point_cloud=False)
+        pc_RM = generate_point_cloud(images_MR, mask_MR, calibration_data, "_mr", display_disparity=False, display_point_cloud=False)
+        print('Finished generating point clouds')
 
 
-# Merge the point clouds using the ICP algorithm (iterated closest points)
-pc_combined = combine_point_clouds(pc_RM, pc_LM, display=False)
+        # Merge the point clouds using the ICP algorithm (iterated closest points)
+        pc_combined = combine_point_clouds(pc_RM, pc_LM, display=False)
 
-# Create mesh from the point cloud
-mesh = create_mesh_poisson(pc_combined, 'pc_combined')
-visualize_mesh(mesh)
+        # Create mesh from the point cloud
+        mesh = create_mesh_poisson(pc_combined, 'pc_combined')
+        visualize_mesh(mesh)
 
-# Save mesh to file
+        # Save mesh to file
 
-# Plot the mesh
+
+        # Plot the mesh
